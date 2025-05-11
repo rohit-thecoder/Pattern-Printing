750273ff19c8a1d43eaec691809d21235ff91d46
Rohit Kumar <rkrohit2059@gmail.com>
2025-05-11 12:22:48 +0530

    Add files via upload

---
+++ StarPyramid.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter no of Rows: ");
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n+1-i; j++){
+            printf(" ");
+        }
+        for(int k=1; k<=2*i-1; k++){
+            printf("*");
+        }
+        for(int l=1; l<=n+1-i; l++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
