3da4f4ae5e32af07464baaceb1b4a00326a59a40
Rohit Kumar <rkrohit2059@gmail.com>
2025-05-11 12:32:54 +0530

    Update StarPyramid.c

--- StarPyramid.c
+++ StarPyramid.c
@@ -4,7 +4,7 @@ int main(){
     printf("Enter no of Rows: ");
     scanf("%d", &n);
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n+1-i; j++){
+        for(int j=1; j<=n-i; j++){
             printf(" ");
         }
         for(int k=1; k<=2*i-1; k++){
