0a7b6937cfc1256f93c1a49a3e0f394cf0b1165c
Rohit Kumar <rkrohit2059@gmail.com>
2025-05-11 12:28:13 +0530

    Update StarPyramid.c

--- StarPyramid.c
+++ StarPyramid.c
@@ -10,7 +10,7 @@ int main(){
         for(int k=1; k<=2*i-1; k++){
             printf("*");
         }
-        for(int l=1; l<=n+1-i; l++){
+        for(int l=1; l<=n-i; l++){
             printf(" ");
         }
         printf("\n");
